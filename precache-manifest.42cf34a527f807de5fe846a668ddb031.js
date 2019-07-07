8e2d8ae70dd33879cb5f26a322efc5fc0b703168
James Dorr <jamesdorr3@gmail.com>
2019-07-06 20:10:30 -0700

    Updates

---
+++ precache-manifest.42cf34a527f807de5fe846a668ddb031.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abf34c7c2c1a4c77f2addf655676a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ebd99806b06d676a3e",
+    "url": "/static/css/main.9b4eb984.chunk.css"
+  },
+  {
+    "revision": "2baa6c00b893f5c2185e",
+    "url": "/static/js/2.9c8a7bb1.chunk.js"
+  },
+  {
+    "revision": "a0ebd99806b06d676a3e",
+    "url": "/static/js/main.96556dad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df200a086a222bc5556f181601306df7",
+    "url": "/static/media/chongqing.df200a08.png"
+  },
+  {
+    "revision": "f6f97a5be9a2a525d4f1aeff295d001d",
+    "url": "/static/media/coach.f6f97a5b.jpg"
+  },
+  {
+    "revision": "0f0422ccb9d57f8fe404baa6d2b55d35",
+    "url": "/static/media/volleyball.0f0422cc.jpeg"
+  }
+]);
