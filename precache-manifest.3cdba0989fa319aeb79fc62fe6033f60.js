ee4236cd1e85f40c20fbf6c1d8d139cb7bc0f7dd
James Dorr <jamesdorr3@gmail.com>
2019-09-09 18:28:03 -0700

    Updates

---
+++ precache-manifest.3cdba0989fa319aeb79fc62fe6033f60.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29379062321adac456f4d4f5f35ba5a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a89af875d0c2df3e5ba3",
+    "url": "/static/css/main.c7de722e.chunk.css"
+  },
+  {
+    "revision": "6b39f0e0df67e22d60a5",
+    "url": "/static/js/2.048d35a6.chunk.js"
+  },
+  {
+    "revision": "a89af875d0c2df3e5ba3",
+    "url": "/static/js/main.555c15c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f20ba0cd099b7e9ff40be1c1b0d8ace0",
+    "url": "/static/media/chongqing-big.f20ba0cd.png"
+  },
+  {
+    "revision": "df200a086a222bc5556f181601306df7",
+    "url": "/static/media/chongqing.df200a08.png"
+  },
+  {
+    "revision": "f6f97a5be9a2a525d4f1aeff295d001d",
+    "url": "/static/media/coach.f6f97a5b.jpg"
+  },
+  {
+    "revision": "0bb789f78cf2f00bcd79c97049b83bba",
+    "url": "/static/media/css.0bb789f7.jpg"
+  },
+  {
+    "revision": "00a91b267e50bf017168b995387b8929",
+    "url": "/static/media/html.00a91b26.jpg"
+  },
+  {
+    "revision": "0506e134e607e73093a79bf8c50f6d41",
+    "url": "/static/media/javascript.0506e134.png"
+  },
+  {
+    "revision": "bf64cc81007fbb2922333665f611798d",
+    "url": "/static/media/postgres.bf64cc81.png"
+  },
+  {
+    "revision": "e4b772f4e234cec8f795709cbb47c35f",
+    "url": "/static/media/rails.e4b772f4.png"
+  },
+  {
+    "revision": "37e8896644a5a7b4901dd375f33e1c2d",
+    "url": "/static/media/react.37e88966.png"
+  },
+  {
+    "revision": "ddf6bdc8e1469d158a71de3b309f6846",
+    "url": "/static/media/redux.ddf6bdc8.png"
+  },
+  {
+    "revision": "a2df0158bdcb86fad97a79537fd8bde5",
+    "url": "/static/media/sql.a2df0158.jpg"
+  },
+  {
+    "revision": "7cc3d43248b5f361f3c9566fa3f6b00f",
+    "url": "/static/media/sqlite.7cc3d432.png"
+  },
+  {
+    "revision": "0f0422ccb9d57f8fe404baa6d2b55d35",
+    "url": "/static/media/volleyball.0f0422cc.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e3e71ffa644932259ec98c23427db7a.js"
+  "/precache-manifest.3cdba0989fa319aeb79fc62fe6033f60.js"
 );
 
 self.addEventListener('message', (event) => {
